0ce79436292e26c5fd8d12c45e9d60a6c5d2a73c
Olicleiton <olicaldana@hotmail.com>
2020-11-12 21:55:15 -0300

    salvando a sessao no sistema

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,15 +2,37 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	
+	@ManyToOne
 	private Filme filme;
+	
+	/**
+	* @deprecated hibernate only
+	*/
 
+	public Sessao() {
+		
+	}
+	
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
-		this.filme = filme;
+		this.filme = filme; 
 		this.sala = sala;
 	}
 
@@ -26,26 +48,25 @@ public class Sessao {
 		return horario;
 	}
 
-	public void setHorario(LocalTime horario) {
-		this.horario = horario;
-	}
-
 	public Sala getSala() {
 		return sala;
-	}
-
-	public void setSala(Sala sala) {
-		this.sala = sala;
 	}
 
 	public Filme getFilme() {
 		return filme;
 	}
 
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
+
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+
 	public void setFilme(Filme filme) {
 		this.filme = filme;
 	}
-	
-	
 
+	
 }
